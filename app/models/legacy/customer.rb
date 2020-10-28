4c3305fbb5df5744f5b2291441aa69f0325dc13c
Taras Gaiduchyk <thaiduchyk@gmail.com>
2020-10-28 13:40:29 +0200

    Configure and test multiple db connections

---
+++ app/models/legacy/customer.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Legacy
+  class Customer < LegacyBase
+    self.table_name = 'gc_customers'
+  end
+end
---
+++ app/models/legacy_base.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class LegacyBase < ApplicationRecord
+  self.abstract_class = true
+
+  connects_to database: { writing: :legacy, reading: :legacy }
+end
