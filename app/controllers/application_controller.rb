41fdc475ad5b896f1ebe4ec79dd1652d8014a05f
Taras Gaiduchyk <tgaiduchyk@roku.com>
2021-12-22 10:14:01 +0200

    New version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
   rescue_from Errors::ApiBaseError, with: :render_error
 
   def version
-    render json: { api_version: '1.0.3' }
+    render json: { api_version: '1.0.4' }
   end
 
   private
